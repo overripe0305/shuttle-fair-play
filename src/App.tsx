80e4ca6b4e629a2ab2c5319a30c9eec82e831268
overripe0305 <64093157+overripe0305@users.noreply.github.com>
2025-07-25 07:50:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import PlayerManagement from "./pages/PlayerManagement";
+// ...inside <Routes>
+<Route path="/players" element={<PlayerManagement />} />
 
 const queryClient = new QueryClient();
 
