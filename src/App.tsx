90f288109584e451de9768647bdb65a360fe1780
overripe0305 <64093157+overripe0305@users.noreply.github.com>
2025-07-25 08:01:30 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+<Routes>
+  <Route path="/" element={<Index />} />
+  <Route path="/players" element={<PlayerManagement />} />
+  <Route path="*" element={<NotFound />} />
+</Routes>
+
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
